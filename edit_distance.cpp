51e28e27adfb8f0439b9878b47074612aff78e43
David Manda <davidmanda1998@gmail.com>
2019-11-24 19:09:42 +0000

    Add buffer elem to strings

--- edit_distance.cpp
+++ edit_distance.cpp
@@ -30,7 +30,7 @@ int get_edit_dist(const int N, const int M, const string& s0, const string& s1){
 
     for(int i = 1; i <= N; i++){
         for(int j = 1; j <= M; j++){
-            dyn[i][j] = min(dyn[i-1][j] + 1, dyn[i][j-1] + 1, dyn[i-1][j-1] + (s0[i-1] != s1[j-1]));
+            dyn[i][j] = min(dyn[i-1][j] + 1, dyn[i][j-1] + 1, dyn[i-1][j-1] + (s0[i] != s1[j]));
         }
     }
     return dyn[N][M];
@@ -40,7 +40,8 @@ int main() {
     ifstream fin("input.in");
     ofstream fout("output.out");
     int N, M;
-    string s0, s1;
+    // The first element in the strings is a buffer element for use of implementation
+    string s0 = "0", s1 = "0";
     fin >> N >> M;
     read_string(fin, N, s0);
     read_string(fin, M, s1);
