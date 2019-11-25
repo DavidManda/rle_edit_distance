3128f22a30daaa67f31dbe5b5a334ae7bc28bae2
David Manda <davidmanda1998@gmail.com>
2019-11-25 08:24:18 +0000

    Fix indeces and add support for multiple tests

--- LCS.cpp
+++ LCS.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include "rle_string.h"
+#include <string.h>
 
 using namespace std;
 
@@ -118,8 +119,8 @@ void forced_path_update(const vector<RLE_run> s0, const vector<RLE_run> s1, int 
 }
 
 void print_2d_vect(vector< vector<int> > vect){
-    for(int i = 0; i < vect.size(); i++){
-        for(int j = 0; j < vect[i].size(); j++){
+    for(int i = 1; i < vect.size(); i++){
+        for(int j = 1; j < vect[i].size(); j++){
             cout << vect[i][j] << ' ';
         }
         cout << '\n';
@@ -141,8 +142,13 @@ int get_rle_lcs(const vector<RLE_run> s0, const vector<RLE_run> s1){
             }
             // match block
             int d = min(s0[i].len, s1[j].len);
-            dyn[i][j] = max(dyn[i-1][j-1] + d, dyn[i][j], max(dyn[i-1][j], dyn[i][j-1]));
+            int curr_val = dyn[i][j];
+            // start forced path at i j
+            dyn[i][j] = dyn[i-1][j-1] + d;
             forced_path_update(s0, s1, i, j, dyn);
+
+            dyn[i][j] = max(curr_val, dyn[i][j], max(dyn[i-1][j], dyn[i][j-1]));
+            print_2d_vect(dyn);
         }
     }
 
@@ -153,19 +159,28 @@ int main(){
     ifstream fin("input.in");
     ofstream fout("output.out");
 
-    int N, M;
-    string s0 = "0", s1 = "0";
-    fin >> N >> M;
-    read_string(fin, N, s0);
-    read_string(fin, M, s1);
+    int T, N, M;
+    fin >> T;
+    int nr_fail = 0;
+    for(int i = 0; i < T; i++){
+        string s0 = "0", s1 = "0";
+        fin >> N >> M;
+        read_string(fin, N, s0);
+        read_string(fin, M, s1);
 
-    int sol = get_LCS(N, M, s0, s1);
+        int sol = get_LCS(N, M, s0, s1);
 
-    RLE_string_helper helper;
-    vector<RLE_run> rle_s0 = helper.get_rle_string(s0);
-    vector<RLE_run> rle_s1 = helper.get_rle_string(s1);
-    int rle_sol = get_rle_lcs(rle_s0, rle_s1);
-    cout << sol <<'\n';
-    cout << rle_sol <<'\n';
+        RLE_string_helper helper;
+        vector<RLE_run> rle_s0 = helper.get_rle_string(s0);
+        vector<RLE_run> rle_s1 = helper.get_rle_string(s1);
+        int rle_sol = get_rle_lcs(rle_s0, rle_s1);
+        if(rle_sol != sol){
+            nr_fail ++;
+            cout << "Failed test " << i << '\n';
+            cout << "Expected " << sol << " but received " << rle_sol << '\n';
+            cout << "Strings are:\n" << s0.substr(1, s0.length()) << "\n" << s1.substr(1,s1.length()) << '\n';
+        }
+    }
+    cout << "Passed " << T - nr_fail << "/" << T <<" tests!\n";
     return 0;
 }
