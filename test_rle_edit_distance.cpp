94e75f916c3375b9e6aa968ba0eb441495bd89ab
David Manda <davidmanda1998@gmail.com>
2020-03-08 12:06:34 +0000

    Add tests for join edit distance

--- test_rle_edit_distance.cpp
+++ test_rle_edit_distance.cpp
@@ -26,44 +26,68 @@ void test_get_val_at_coord(){
   assert(val == 0);
 }
 
-// void test_join(){
-  // BST t_l,t_r, t;
-  // t = *join(&t_l, &t_r);
+void test_join_empty_trees(){
+  BST t_l,t_r, t;
+  t = join(t_l, t_r);
 
-  // assert(t->root == NULL);
-  // Segment s_0011 = Segment(Point(0,0), Point(1,1));
-  // t->insert(s_0011);
-  // std::cout<<t<<' '<<t_l<<' '<<t_r<<'\n';
-  // std::cout<<t->root<<' '<<t_l->root<<' '<<t_r->root<<'\n';
-  // assert(t->root->segm == s_0011);
-  // assert(t_l->root == NULL && t_r->root == NULL);
+  assert(t.root == NULL);
+}
 
-  // BST::free(t);t = NULL;
-  // t_l->insert(s_0011);
-  // t = join(t_l, t_r);
-  // assert(t->root->segm == s_0011);
+void test_join_left_empty(){
+  BST t_l,t_r,t;
+  Segment s_0011 = Segment(Point(0,0), Point(1,1));
+  t_r.insert(s_0011);
+  t = join(t_l, t_r);
 
-  // BST::free(t); t = NULL;
-  // BST::free(t_l); t_l = NULL;
-  // BST::free(t_r); t_r = NULL;
-  // t_l = new BST(); t_r = new BST();
-  // Segment s_1121(Point(1,1), Point(2,1));
-  // t_r->insert(s_1121);
-  // t = join(t_l, t_r);
-  // TreeNode *min = BST::min(t->root);
-  // TreeNode *max = BST::max(t->root);
-  // assert(min->segm == s_0011);
-  // assert(max->segm == s_1121);
+  assert(t.root->segm == s_0011);
+}
 
-  // BST::free(t);BST::free(t_l);BST::free(t_r); 
-  // t_l = new BST(); t_r = new BST(); 
-  // Segment s_1122(Point(1,1), Point(2,2));
-  // t_l->insert(s_0011);
-  // t_r->insert(s_1122);
-  // t = join(t_l, t_r);
-  // assert(t->root->segm == Segment(Point(0,0), Point(2,2)));
-// }
+void test_join_right_empty(){
+  BST t_l,t_r,t;
+  Segment s_0011(Point(0,0), Point(1,1));
+  t_l.insert(s_0011);
+  t = join(t_l, t_r);
+
+  assert(t.root->segm == s_0011);
+}
+
+void test_join_same_gradient(){
+  BST t_l,t_r,t;
+  Segment s_0011(Point(0,0), Point(1,1));
+  Segment s_1122(Point(1,1), Point(2,2));
+  t_l.insert(s_0011);
+  t_r.insert(s_1122);
+  t = join(t_l, t_r);
+
+  // check that segments were joined as they have the same gradient
+  assert(t.root->segm == Segment(Point(0,0), Point(2,2)));
+
+}
+
+void test_join_different_gradient(){
+  BST t_l,t_r,t;
+  Segment s_0011(Point(0,0), Point(1,1));
+  Segment s_1121(Point(1,1), Point(2,1));
+  Segment s_2133(Point(2,1), Point(3,3));
+  t_l.insert(s_0011);
+  t_r.insert(s_1121);
+  t_r.insert(s_2133);
+  t = join(t_l, t_r);
+
+  assert(t.root->segm == s_1121);
+  assert(t.root->left->segm == s_0011);
+  assert(t.root->right->segm == s_2133);
+}
+
+void test_join(){
+  test_join_empty_trees();
+  test_join_left_empty();
+  test_join_right_empty();
+  test_join_same_gradient();
+  test_join_different_gradient();
+}
 
 void test_rle_edit_distance(){
   test_get_val_at_coord();
+  test_join();
 }
