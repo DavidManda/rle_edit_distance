4330ec1e08baa15ca7eeefae213407c06ca4f940
David Manda <davidmanda1998@gmail.com>
2020-03-09 19:42:23 +0000

    Add point types to definition

--- point.hpp
+++ point.hpp
@@ -3,9 +3,12 @@
 
 #include <iostream>
 
+enum Point_t { ID, IF, FD, FI, DF, _I, _F, _D, I_, F_, D_ } ;
+
 class Point{
 public:
   double x, y;
+  Point_t type;
   Point(double _x, double _y);
   Point();
   bool operator ==(const Point& s);
