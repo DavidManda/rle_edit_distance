0d764a7fd571a7286a2ebcb163c58707a4188dee
David Manda <davidmanda1998@gmail.com>
2020-03-20 19:24:52 +0000

    overload << for enum

--- point.cpp
+++ point.cpp
@@ -14,6 +14,29 @@ int Point::get_manhattan(Point p1, Point p2){
   return std::abs(p1.x - p2.x) + std::abs(p1.y - p2.y);
 }
 
+std::ostream& operator<<(std::ostream& out, const Point_t value){
+  const char* s = 0;
+#define PROCESS_VAL(p) case(p): s = #p; break;
+  switch(value){
+    PROCESS_VAL(NotInitialised);     
+    PROCESS_VAL(DI);     
+    PROCESS_VAL(ID);
+    PROCESS_VAL(IF);     
+    PROCESS_VAL(FD);     
+    PROCESS_VAL(FI);
+    PROCESS_VAL(DF);     
+    PROCESS_VAL(_I);     
+    PROCESS_VAL(_F);
+    PROCESS_VAL(_D);
+    PROCESS_VAL(I_);     
+    PROCESS_VAL(F_);     
+    PROCESS_VAL(D_);
+  }
+#undef PROCESS_VAL
+
+    return out << s;
+}
+
 std::string Point::to_string()
 {
   std::stringstream ss;
--- point.hpp
+++ point.hpp
@@ -5,6 +5,8 @@
 
 enum Point_t { NotInitialised, DI, ID, IF, FD, FI, DF, _I, _F, _D, I_, F_, D_ } ;
 
+std::ostream& operator<<(std::ostream& out, const Point_t value);
+
 class Point{
 public:
   double x, y;
