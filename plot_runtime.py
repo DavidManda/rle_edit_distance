0893c4e7be27f64cf856a556e30689f4d7593790
David Manda <davidmanda1998@gmail.com>
2020-04-01 19:17:11 +0300

    change the way we plot

--- plot_runtime.py
+++ plot_runtime.py
@@ -5,28 +5,36 @@ f = open("run_times.out", "r")
 
 data = f.read().split('\n')
 
-xs = []
-naive_runtimes = []
-rle_runtimes = []
-compression_factor = []
+results = []
+operation_cost = 1.0/4000000
 for i in range(1,len(data)-1):
   elem = data[i].split(',')
-  M = int(elem[0])
-  N = int(elem[1])
-  compression_fact0 = float(elem[2])
-  compression_fact1 = float(elem[3])
-  naive_runtime = float(elem[4])
-  rle_runtime = float(elem[5])
-  xs.append(M/compression_fact0)
-  compression_factor.append(compression_fact0 * compression_fact1)
-  naive_runtimes.append(naive_runtime)
-  rle_runtimes.append(rle_runtime)
+  result = {}
+  result['M'] = int(elem[0])
+  result['N'] = int(elem[1])
+  result['compression_fact0'] = float(elem[2])
+  result['compression_fact1'] = float(elem[3])
+  result['naive_runtime'] = float(elem[4])
+  result['rle_runtime'] = float(elem[5])
+  result['m'] = result['M']/result['compression_fact0']
+  result['n'] = result['N']/result['compression_fact1']
+  result['compressed_size'] = result['m']
+  results.append(result)
 
-aux = [4*i for i in naive_runtimes]
-plt.plot(xs, rle_runtimes, label='Compressed')
-plt.plot(xs, naive_runtimes, label='Naive')
-plt.plot(xs, aux, label='4x naive')
+results.sort(key = lambda x : x['compressed_size'])
+
+naive_runtimes = [result['naive_runtime'] for result in results]
+rle_runtimes = [result['rle_runtime'] for result in results]
+compressed_sizes = [result['compressed_size'] for result in results]
+N2logN2 = [size**2 * math.log(size**2, 2) * operation_cost for size in compressed_sizes]
+NlogN = [size * math.log(size, 2) * operation_cost for size in compressed_sizes]
+plt.plot(compressed_sizes, rle_runtimes, label='Runtime')
+plt.plot(compressed_sizes, N2logN2, label='Complexity - $N^2log(N^2)$')
+# plt.plot(compressed_sizes, NlogN, label='Complexity - $Nlog(N)$')
+# plt.plot(xs, naive_runtimes, label='Naive')
+# plt.plot(xs, aux, label='4x naive')
 # plt.plot(xs, compression_factor, label='Compression factor')
+plt.xlabel('N')
 plt.legend()
 # plt.title('Runtimes for strings comressable by 10x')
 # # plt.plot(times_naive_adjusted, c='y')
