e6936f01e5ab8845e6f71c0c77a6c91e2bd849dc
David Manda <davidmanda1998@gmail.com>
2020-04-28 17:45:11 +0300

    measure runtime minor

--- edit_distance_chen/measure_runtime.cpp
+++ edit_distance_chen/measure_runtime.cpp
@@ -4,19 +4,19 @@
 #include <math.h> 
 #include "edit_distance.hpp"
 #include "helper.hpp"
+#include <sstream>
 
 int main(){
   std::ofstream fout("run_times.out");
   fout<<"M, N, compression factor 1, compression factor 2, naive time, rle time\n";
-  for(int i = 50; i <= 15000; i+=50){
+  for(int i = 1; i < 50; i+=1){
     // height is i and width is i*2
-    std::ifstream fin("../inputs/input"+std::to_string(i) + ".in");
+    std::ifstream fin("../inputs/input" + std::to_string(i) + "x" + std::to_string(i*2) + ".in");
     int M, N;
     fin >> M >> N;
     std::string s0 = "*",s1 = "*";
     helper::read_string(fin, M, s0);
     helper::read_string(fin, N, s1);
-
     RLE_string_helper rle_helper;
     std::vector<RLE_run> rle_s0 = rle_helper.get_rle_string(s0);
     std::vector<RLE_run> rle_s1 = rle_helper.get_rle_string(s1);
@@ -25,6 +25,7 @@ int main(){
     std::clock_t start;
     double naive_time=0, rle_time=0;
     int sol = 0, sol_rle = 0;
+    std::cout<<i<<'\n';
     start = std::clock();
     sol = edit_distance::get_edit_dist(M, N, s0, s1);
     naive_time = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
@@ -40,7 +41,7 @@ int main(){
                 << s1.substr(1, s1.length()) << '\n';
       return 0;
     }
-    std::cout<<i<<'\n';
+    std::cout<<"Solution is:"<<sol_rle<<'\n';
     std::cout<<"Naive time is: "<<naive_time<<'\n';
     std::cout<<"RLE time is: "<<rle_time<<'\n';
     fout<<M<<','<<N<<','<<compression_fact0<<','<<compression_fact1<<','<<naive_time<<','<<rle_time<<'\n';
