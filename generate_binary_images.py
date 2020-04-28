5eedc8c68d87f13cb09a9be7b221bc79a3296194
David Manda <davidmanda1998@gmail.com>
2020-04-28 11:49:00 +0300

    measurements stuff

--- generate_binary_images.py
+++ generate_binary_images.py
@@ -8,9 +8,9 @@ def write_img_to_file(binary_img, out):
     for i in range(len(binary_img)):
         for j in range(len(binary_img[i])):
             if binary_img[i][j] == 0:
-                out.write("0")
+                out.write("0" * repeats)
             else:
-                out.write("1")
+                out.write("1" * repeats)
         out.write("\n")
 
 
@@ -22,6 +22,8 @@ def write_string_to_file(string, out):
             out.write("1")
     out.write("\n")
 
+def duplicate_all_elements(l):
+  return [val for val in l for _ in range(15)]
 
 img1 = cv2.imread("test_image1.png", 0)
 img2 = cv2.imread("test_image2.png", 0)
@@ -34,14 +36,35 @@ binary_img1 = cv2.resize(binary_img1, (200, 100))
 binary_img2 = cv2.resize(binary_img2, (200, 100))
 
 binary_img1 = [elem for sublist in binary_img1 for elem in sublist]
-fixed_size = 2000
+binary_img2 = [elem for sublist in binary_img2 for elem in sublist]
+binary_img1 = binary_img1[10000:30000]
+binary_img2 = binary_img2[10000:30000]
+binary_img1 = duplicate_all_elements(binary_img1)
+binary_img2 = duplicate_all_elements(binary_img2)
 
+# fixed_size = 2000
+# fixed_string = binary_img2[:fixed_size]
 for size in range(50, 15001, 50):
     print(size)
-    string = binary_img1[:size]
+    string1 = binary_img1[:size]
+    string2 = binary_img2[:size]
     filename = "inputs/input{}.in".format(size)
     out = open(filename, "w")
-    out.write("{} {}\n".format(size, fixed_size))
-    write_string_to_file(string, out)
+    out.write("{} {}\n".format(size, size))
+    write_string_to_file(string1, out)
     out.write("\n")
-    write_img_to_file(binary_img2, out)
+    write_string_to_file(string2, out)
+repeats = 1
+# for size in range(1,100):
+#   height = size
+#   width = size * 2
+#   dim = (width, height)
+#   res_img1 = cv2.resize(binary_img1,dim)
+#   res_img2 = cv2.resize(binary_img2,dim)
+#   filename = "inputs/input{}x{}.in".format(height, width)
+#   out = open(filename, "w")
+#   out.write("{} {}\n".format(width * height * repeats, width * height *repeats))
+#   write_img_to_file(res_img1, out)
+#   out.write("\n")
+#   write_img_to_file(res_img2, out)
+
--- plot_runtime.py
+++ plot_runtime.py
@@ -6,7 +6,7 @@ f = open("run_times.out", "r")
 data = f.read().split('\n')
 
 results = []
-operation_cost = 1.0/4500
+operation_cost = 1.0/5000000
 for i in range(1,len(data)-1):
   elem = data[i].split(',')
   result = {}
@@ -21,24 +21,23 @@ for i in range(1,len(data)-1):
   result['compressed_size'] = result['m']
   results.append(result)
 
-results.sort(key = lambda x : x['compressed_size'])
+# results.sort(key = lambda x : x['compressed_size'])
 
 compressed_sizes = [result['compressed_size'] for result in results]
 rle_runtimes = [result['rle_runtime'] for result in results]
-mlogm = [operation_cost * m * math.log(m, 2) for m in compressed_sizes]
-plt.plot(compressed_sizes, rle_runtimes, label='Compressed')
-plt.plot(compressed_sizes, mlogm, label='$mlog(m)$')
+mnlogmn = [operation_cost * m**2 * math.log(m**2, 2) for m in compressed_sizes]
 
-# sizes = [result['M'] for result in results]
-# naive_runtimes = [result['naive_runtime'] for result in results]
-# plt.plot(sizes, naive_runtimes, label='Naive')
-# plt.plot(sizes, naive_runtimes_penalised, label='Naive multiplied by $log(n^2)$')
-# plt.plot(sizes, rle_runtimes, label='Compressed')
+# plt.plot(compressed_sizes, rle_runtimes, label='Compressed runtime')
+# plt.plot(compressed_sizes, mnlogmn, label='$mnlog(mn)$')
+sizes = [result['M'] for result in results]
+naive_runtimes = [result['naive_runtime'] for result in results]
+plt.plot(sizes, naive_runtimes, label='Naive')
+plt.plot(sizes, rle_runtimes, label='Compressed')
 # plt.plot(compressed_sizes, NlogN, label='Complexity - $Nlog(N)$')
 # plt.plot(xs, aux, label='4x naive')
 # plt.plot(xs, compression_factor, label='Compression factor')
 plt.xlabel('N')
 plt.ylabel('seconds')
 plt.legend()
-plt.title('Runtimes with one string fixed with compressed length 735')
+# plt.title('Runtimes with one string fixed')
 plt.show()
